818b679f66c4c054f3d418056cc15d3818a0de81
edvinasvencevicius <edviz.nzn@gmail.com>
2024-04-08 22:20:03 +0300

    2 praktika - edvinas vencevicius / counter

--- app/src/test/java/com/example/edvinasvencevicius_2praktika/ExampleUnitTest.java
+++ app/src/test/java/com/example/edvinasvencevicius_2praktika/ExampleUnitTest.java
@@ -1,17 +1,49 @@
 package com.example.edvinasvencevicius_2praktika;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void CountWords_Text() {
+        String text = "Test phrase";
+        int expectedWordCount = 5;
+        assertEquals(expectedWordCount, WordCounter.countWords(text));
+    }
+
+    @Test
+    public void CountWords_EmptyText() {
+        String text = "";
+        int expectedWordCount = 0;
+        assertEquals(expectedWordCount, WordCounter.countWords(text));
+    }
+
+    @Test
+    public void CountWords_Spaces() {
+        String text = "     ";
+        int expectedWordCount = 0;
+        assertEquals(expectedWordCount, WordCounter.countWords(text));
+    }
+
+    @Test
+    public void CountWords_Tabs() {
+        String text = "\t\t\t\t\t";
+        int expectedWordCount = 0;
+        assertEquals(expectedWordCount, WordCounter.countWords(text));
+    }
+
+    @Test
+    public void CountCharacters_Text() {
+        String text = "Test phrase";
+        int expectedCharacterCount = 19;
+        assertEquals(expectedCharacterCount, WordCounter.countCharacters(text));
+    }
+
+    @Test
+    public void CountCharacters_EmptyText() {
+        String text = "";
+        int expectedCharacterCount = 0;
+        assertEquals(expectedCharacterCount, WordCounter.countCharacters(text));
     }
 }
